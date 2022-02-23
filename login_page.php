713109f3c406012ba7be4a0da6ad1f1db2660d5f
souradeepsamadder1@gmail.com <souradeepsamadder1@gmail.com>
2022-02-24 00:58:10 +0530

    login page

--- login_page.php
+++ login_page.php
@@ -22,8 +22,8 @@
             </div>
             <br>
             <div class="cote">
-                <h3 class="word">
-                    welcome welcome welcome welcome welcome welcome welcome welcome welcome welcome
+                <h3 class="word" id="msg" style="color:#ff0000">
+                    
                 </h3>
             </div>
             <div class="form_box">
@@ -40,14 +40,10 @@
                         <div class="container"><br>
                             <h3 style="font-size: 12px; text-align: left;">Enter OTP</h3>
                             <div class="userInput">
-                                <input type="text" id='ist' maxlength="1" minlength="1"
-                                    onkeyup="clickEvent(this,'sec')">
-                                <input type="text" id="sec" maxlength="1" minlength="1"
-                                    onkeyup="clickEvent(this,'third')">
-                                <input type="text" id="third" maxlength="1" minlength="1"
-                                    onkeyup="clickEvent(this,'fourth')">
-                                <input type="text" id="fourth" maxlength="1" minlength="1"
-                                    onkeyup="clickEvent(this,'fifth')">
+                                <input type="text" id='ist' maxlength="1" minlength="1" onkeyup="clickEvent(this,'sec')">
+                                <input type="text" id="sec" maxlength="1" minlength="1" onkeyup="clickEvent(this,'third')">
+                                <input type="text" id="third" maxlength="1" minlength="1" onkeyup="clickEvent(this,'fourth')">
+                                <input type="text" id="fourth" maxlength="1" minlength="1" onkeyup="clickEvent(this,'fifth')">
                             </div>
                         </div>
                     </div>
@@ -64,7 +60,7 @@
                     <div>
                         <p style="font-size: 12px;" class="or">OR</p>
                         <p style="font-size: 12px;">Do not have an account?</p>
-                        <button type="submit" class="register_btn">Register</button>
+                        <button type="submit" class="register_btn"><a style="color:#fff;" href="registration.php">Register</a></button>
                     </div>
 
                 </form>
@@ -80,6 +76,22 @@
             document.getElementById(last).focus();
         }
     }
+    var r_text = new Array();
+    r_text[0] = "BE A HERO GIVE BLOOD";
+    r_text[1] = "EVERYONE MAY BECOME A SAVIOR";
+    r_text[2] = "GIVE BLOOD GIVE LIFE ";
+    r_text[3] = "THERE IS NO SUBSTUTUTE FOR BLOOD IT ONLY COMES FROM GENEROUS DONNER ";
+    r_text[4] = "NEVER REFUSE TO DONATE BLOOD IF YOU CAN AS YOU MY BE THE NEXT NEEDY";
+    r_text[5] = "DONATE YOUR BOLLD AND MAKE A DINCEFFERE";
+    r_text[6] = "YOU DONT.N HAVE TO BE A DOCTOR TO SAVE LIVES";
+    r_text[7] = "GIVE THE GIFT OF LIFE DONATE BLOOD";
+    r_text[8] = "WE NEED EACH OTHER DONATE BLOOD";
+    r_text[9] = "PROUD TO BE A BLOOD DONOR";
+    r_text[10] = "1 PAIN CAN SAVE 3 LIVES GIVE BLOOD GIVVE LIFE";
+    var i = Math.floor(11 * Math.random())
+    document.getElementById("msg").innerHTML = r_text[i]
+    console.log(r_text[i])
+    // alert("aoura")
 </script>
 
 </html>
